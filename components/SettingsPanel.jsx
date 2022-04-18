5d897327bb74dd910c6a508f7ad17fda1a063466
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-17 22:35:24 -0400

    Settings component, wip

--- components/SettingsPanel.jsx
+++ components/SettingsPanel.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import PanelSection from "@/components/Settings/PanelSection";
+import CanvasBackground from "@/components/Settings/CanvasBackground";
+import CanvasSize from "@/components/Settings/CanvasSize";
 
 export default function SettingsPanel() {
-  return <div className="settings">SettingsPanel</div>;
+  return (
+    <div className="settings">
+      <PanelSection title="Canvas">
+        <CanvasSize />
+        <CanvasBackground />
+      </PanelSection>
+      <PanelSection title="Variants" />
+      <PanelSection title="Direction" />
+    </div>
+  );
 }
