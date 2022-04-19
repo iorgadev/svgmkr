792df1fc6bf510e04aaf7b6cb6ca8f268fa0b669
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-18 20:58:40 -0400

    add new state params

--- store/index.js
+++ store/index.js
@@ -3,13 +3,15 @@ import { atom } from "jotai";
 export const settingsAtom = atom({
   width: 800,
   height: 600,
-  backgroundColor: "#fff",
+  backgroundColor: "#3a0073",
   prevBackgroundColor: "#fff",
-  fillColor: "#000",
+  fillColor: "#8000ff",
+  fillType: "solid",
   strokeColor: "#000",
   strokeWidth: 1,
   lineCap: "round",
   lineJoin: "round",
   count: 3,
   size: 10,
+  disparity: 0,
 });
