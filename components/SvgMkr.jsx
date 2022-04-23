40964c1bfc0cef57f95dd27ee0b966b5d2a30d97
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-23 18:14:17 -0400

    remove static Circles

--- components/SvgMkr.jsx
+++ components/SvgMkr.jsx
@@ -1,23 +1,19 @@
-import { useState } from "react";
 import Canvas from "@/components/Canvas";
 import SettingsPanel from "@/components/SettingsPanel";
 import SvgContainer from "@/components/SvgContainer";
 import CircleLayer from "./Circle/CircleLayer";
 import Circle from "./Circle/Circle";
-import { useAtom } from "jotai";
-import { settingsAtom } from "@/store/index";
 
 export default function SvgMkr() {
-  const [settings] = useAtom(settingsAtom);
   return (
     <div className="wrapper">
       <SettingsPanel />
       <Canvas>
         <SvgContainer>
-          <CircleLayer>
+          {/* <CircleLayer>
             <Circle />
             <Circle circleRadius="200" x="600" y="350" />
-          </CircleLayer>
+          </CircleLayer> */}
         </SvgContainer>
       </Canvas>
     </div>
