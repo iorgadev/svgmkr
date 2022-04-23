637e940204eac25dc4226b5ea1c99cc4b3b0d9f8
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-23 18:16:02 -0400

    refactor to use Slider component

--- components/Settings/SizeAmount.jsx
+++ components/Settings/SizeAmount.jsx
@@ -1,25 +1,52 @@
-import { useState, useEffect, useRef } from "react";
 import { useAtom } from "jotai";
 import { settingsAtom } from "@/store/index";
-import Option from "@/components/Option";
+import Slider from "@/components/Option/Slider";
 import { ArrowsExpandIcon } from "@heroicons/react/outline";
 
 export default function CountAmount() {
   const [settings, setSettings] = useAtom(settingsAtom);
-  const [count, setCount] = useState(settings.count);
+
+  const updateSettingsCount = (value) => {
+    setSettings((prev) => ({ ...prev, size: value }));
+  };
 
   return (
-    <Option title="Size">
-      <div className="rounded-full p-1.5 bg-neutral-700">
-        <ArrowsExpandIcon className="option__icon-round icon-small" />
-      </div>
-      <div className="relative option__slider w-full items-center justify-center flex">
-        <div className="w-full h-1 bg-neutral-500 rounded-full"></div>
-        <div className="absolute w-5 h-5 left-7 rounded-full bg-neutral-300 border-4 border-neutral-500"></div>
-      </div>
-      <div className="rounded-full p-1.5 bg-neutral-700">
-        <ArrowsExpandIcon className="option__icon-round" />
-      </div>
-    </Option>
+    <Slider
+      LeftIcon={ArrowsExpandIcon}
+      RightIcon={ArrowsExpandIcon}
+      title="Size"
+      value={settings.size}
+      min={1}
+      max={100}
+      steps={1}
+      onChange={updateSettingsCount}
+      smallIcon={true}
+    />
   );
 }
+
+// import { useState, useEffect, useRef } from "react";
+// import { useAtom } from "jotai";
+// import { settingsAtom } from "@/store/index";
+// import Option from "@/components/Option";
+// import { ArrowsExpandIcon } from "@heroicons/react/outline";
+
+// export default function CountAmount() {
+//   const [settings, setSettings] = useAtom(settingsAtom);
+//   const [count, setCount] = useState(settings.count);
+
+//   return (
+//     <Option title="Size">
+//       <div className="rounded-full p-1.5 bg-neutral-700">
+//         <ArrowsExpandIcon className="option__icon-round icon-small" />
+//       </div>
+//       <div className="relative flex items-center justify-center w-full option__slider">
+//         <div className="w-full h-1 rounded-full bg-neutral-500"></div>
+//         <div className="absolute w-5 h-5 border-4 rounded-full left-7 bg-neutral-300 border-neutral-500"></div>
+//       </div>
+//       <div className="rounded-full p-1.5 bg-neutral-700">
+//         <ArrowsExpandIcon className="option__icon-round" />
+//       </div>
+//     </Option>
+//   );
+// }
--- components/Settings/SizeVariation.jsx
+++ components/Settings/SizeVariation.jsx
@@ -1,25 +1,52 @@
-import { useState, useEffect, useRef } from "react";
 import { useAtom } from "jotai";
 import { settingsAtom } from "@/store/index";
-import Option from "@/components/Option";
+import Slider from "@/components/Option/Slider";
 import { ShareIcon, TemplateIcon } from "@heroicons/react/outline";
 
 export default function CountAmount() {
   const [settings, setSettings] = useAtom(settingsAtom);
-  const [count, setCount] = useState(settings.count);
+
+  const updateSettingsCount = (value) => {
+    console.log("updateSettingsCount", value);
+    setSettings((prev) => ({ ...prev, sizeVariation: value }));
+  };
 
   return (
-    <Option title="Size Variation">
-      <div className="rounded-full p-1.5 bg-neutral-700">
-        <ShareIcon className="option__icon-round" />
-      </div>
-      <div className="option__slider relative w-full items-center justify-center flex">
-        <div className="w-full h-1 bg-neutral-500 rounded-full"></div>
-        <div className="absolute left-24 w-5 h-5 rounded-full bg-neutral-300 border-4 border-neutral-500"></div>
-      </div>
-      <div className="rounded-full p-1.5 bg-neutral-700">
-        <TemplateIcon className="option__icon-round" />
-      </div>
-    </Option>
+    <Slider
+      LeftIcon={ShareIcon}
+      RightIcon={TemplateIcon}
+      title="Size Variation"
+      value={settings.sizeVariation}
+      min={0}
+      max={100}
+      steps={2}
+      onChange={updateSettingsCount}
+    />
   );
 }
+
+// import { useState, useEffect, useRef } from "react";
+// import { useAtom } from "jotai";
+// import { settingsAtom } from "@/store/index";
+// import Option from "@/components/Option";
+// import { ShareIcon, TemplateIcon } from "@heroicons/react/outline";
+
+// export default function CountAmount() {
+//   const [settings, setSettings] = useAtom(settingsAtom);
+//   const [count, setCount] = useState(settings.count);
+
+//   return (
+//     <Option title="Size Variation">
+//       <div className="rounded-full p-1.5 bg-neutral-700">
+//         <ShareIcon className="option__icon-round" />
+//       </div>
+//       <div className="relative flex items-center justify-center w-full option__slider">
+//         <div className="w-full h-1 rounded-full bg-neutral-500"></div>
+//         <div className="absolute w-5 h-5 border-4 rounded-full left-24 bg-neutral-300 border-neutral-500"></div>
+//       </div>
+//       <div className="rounded-full p-1.5 bg-neutral-700">
+//         <TemplateIcon className="option__icon-round" />
+//       </div>
+//     </Option>
+//   );
+// }
