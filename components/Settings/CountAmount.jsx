7b054593af9228dd1ed224a07e7944713d36ad10
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-23 18:15:21 -0400

    clean up code

--- components/Settings/CountAmount.jsx
+++ components/Settings/CountAmount.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useAtom } from "jotai";
 import { settingsAtom } from "@/store/index";
 import Slider from "@/components/Option/Slider";
@@ -18,39 +17,9 @@ export default function CountAmount() {
       title="Count"
       value={settings.count}
       min={1}
-      max={100}
+      max={300}
+      steps={1}
       onChange={updateSettingsCount}
     />
   );
 }
-
-// import { useState, useEffect, useRef } from "react";
-// import { useAtom } from "jotai";
-// import { settingsAtom } from "@/store/index";
-// import Option from "@/components/Option";
-// import { PlusIcon, MinusIcon } from "@heroicons/react/outline";
-
-// export default function CountAmount() {
-//   const [settings, setSettings] = useAtom(settingsAtom);
-//   const [count, setCount] = useState(settings.count);
-
-//   return (
-//     <Option title="Count">
-//       <div className="rounded-full p-1.5 bg-neutral-700">
-//         <MinusIcon className="option__icon-round" />
-//       </div>
-//       <div className="flex items-center justify-center w-full option__slider">
-//         <input
-//           type="range"
-//           min="1"
-//           max="10"
-//           value={count}
-//           onChange={(e) => setCount(e.target.value)}
-//         />
-//       </div>
-//       <div className="rounded-full p-1.5 bg-neutral-700">
-//         <PlusIcon className="option__icon-round" />
-//       </div>
-//     </Option>
-//   );
-// }
