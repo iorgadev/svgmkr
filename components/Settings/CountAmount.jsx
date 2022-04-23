7233246f224f309b377ff58ea3a657b377e8e602
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-23 16:46:07 -0400

    refactor CountAmount to use Slider component

--- components/Settings/CountAmount.jsx
+++ components/Settings/CountAmount.jsx
@@ -1,25 +1,56 @@
-import { useState, useEffect, useRef } from "react";
+import { useState } from "react";
 import { useAtom } from "jotai";
 import { settingsAtom } from "@/store/index";
-import Option from "@/components/Option";
-import { PlusIcon, MinusIcon } from "@heroicons/react/outline";
+import Slider from "@/components/Option/Slider";
+import { MinusIcon, PlusIcon } from "@heroicons/react/outline";
 
 export default function CountAmount() {
   const [settings, setSettings] = useAtom(settingsAtom);
-  const [count, setCount] = useState(settings.count);
+
+  const updateSettingsCount = (value) => {
+    setSettings((prev) => ({ ...prev, count: value }));
+  };
 
   return (
-    <Option title="Count">
-      <div className="rounded-full p-1.5 bg-neutral-700">
-        <MinusIcon className="option__icon-round" />
-      </div>
-      <div className="option__slider w-full items-center justify-center flex">
-        <div className="w-full h-1 bg-neutral-500 rounded-full"></div>
-        <div className="absolute w-5 h-5 rounded-full bg-neutral-300 border-4 border-neutral-500"></div>
-      </div>
-      <div className="rounded-full p-1.5 bg-neutral-700">
-        <PlusIcon className="option__icon-round" />
-      </div>
-    </Option>
+    <Slider
+      LeftIcon={MinusIcon}
+      RightIcon={PlusIcon}
+      title="Count"
+      value={settings.count}
+      min={1}
+      max={100}
+      onChange={updateSettingsCount}
+    />
   );
 }
+
+// import { useState, useEffect, useRef } from "react";
+// import { useAtom } from "jotai";
+// import { settingsAtom } from "@/store/index";
+// import Option from "@/components/Option";
+// import { PlusIcon, MinusIcon } from "@heroicons/react/outline";
+
+// export default function CountAmount() {
+//   const [settings, setSettings] = useAtom(settingsAtom);
+//   const [count, setCount] = useState(settings.count);
+
+//   return (
+//     <Option title="Count">
+//       <div className="rounded-full p-1.5 bg-neutral-700">
+//         <MinusIcon className="option__icon-round" />
+//       </div>
+//       <div className="flex items-center justify-center w-full option__slider">
+//         <input
+//           type="range"
+//           min="1"
+//           max="10"
+//           value={count}
+//           onChange={(e) => setCount(e.target.value)}
+//         />
+//       </div>
+//       <div className="rounded-full p-1.5 bg-neutral-700">
+//         <PlusIcon className="option__icon-round" />
+//       </div>
+//     </Option>
+//   );
+// }
