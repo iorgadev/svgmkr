1e9a1b72e6fe2a0a22985d13fd9cc79d0a7ad37c
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-23 22:46:47 -0400

    update component to show actual values

--- components/Settings/CanvasSize.jsx
+++ components/Settings/CanvasSize.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import Option from "@/components/Option";
+import { useAtom } from "jotai";
+import { settingsAtom } from "@/store/index";
 
 export default function CanvasSize() {
+  const [settings] = useAtom(settingsAtom);
   return (
     <Option>
       <div className="canvassize">
         <div className="canvassize__container">
           <div className="canvassize__option">
             <div className="canvassize__type">W</div>
-            <input type="text" value="900" onChange={() => {}} />
+            <input type="text" value={settings.width} onChange={() => {}} />
             <div className="canvassize__measure">px</div>
           </div>
 
           <div className="canvassize__option">
             <div className="canvassize__type">H</div>
-            <input type="text" value="600" onChange={() => {}} />
+            <input type="text" value={settings.height} onChange={() => {}} />
             <div className="canvassize__measure">px</div>
           </div>
         </div>
