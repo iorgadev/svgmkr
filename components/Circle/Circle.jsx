535ba14bf78c9938ad90830af98416ef10ba2f23
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-23 22:16:53 -0400

    update Circle to include stroke

--- components/Circle/Circle.jsx
+++ components/Circle/Circle.jsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
+import { useAtom } from "jotai";
+import { settingsAtom } from "@/store/index";
 
 export default function Circle({ circleRadius = 100, x = 200, y = 200 }) {
-  const [radius, setRadius] = useState(circleRadius);
+  const [settings, setSettings] = useAtom(settingsAtom);
+  const [radius, setRadius] = useState(circleRadius); // might want to resize later
   const [position, setPosition] = useState({ x: x, y: y });
-  return <circle r={circleRadius} cx={position.x} cy={position.y} />;
+  return (
+    <circle
+      r={circleRadius}
+      cx={position.x}
+      cy={position.y}
+      stroke={settings.strokeColor}
+      strokeWidth={settings.strokeWidth}
+    />
+  );
 }
