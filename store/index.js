30d6593b033b6bd2e454f7cc4d02a913b7886bf6
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-25 19:53:00 -0400

    remove jotai from store

--- store/index.js
+++ store/index.js
@@ -1,26 +1,4 @@
-import { atom } from "jotai";
 import create from "zustand";
-
-export const settingsAtom = atom({
-  width: 800,
-  minWidth: 400,
-  height: 600,
-  minHeight: 300,
-  backgroundColor: "#3a0073",
-  prevBackgroundColor: "#fff",
-  fillColor: "#8000ff",
-  fillType: "solid",
-  strokeColor: "#edd9ff",
-  strokeWidth: 1,
-  lineCap: "round",
-  lineJoin: "round",
-  count: 3,
-  size: 10,
-  minSizePx: 1,
-  sizeVariation: 1,
-  disparity: 0,
-  overlap: true,
-});
 
 const useStore = create((set) => ({
   width: 800,
