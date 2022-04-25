dc88db1995bdd65f439aa10b4fb1608d2a40b05d
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-25 16:05:45 -0400

    clean up code

--- components/SvgMkr.jsx
+++ components/SvgMkr.jsx
@@ -1,20 +1,13 @@
 import Canvas from "@/components/Canvas";
 import SettingsPanel from "@/components/SettingsPanel";
 import SvgContainer from "@/components/SvgContainer";
-import CircleLayer from "./Circle/CircleLayer";
-import Circle from "./Circle/Circle";
 
 export default function SvgMkr() {
   return (
     <div className="wrapper">
       <SettingsPanel />
       <Canvas>
-        <SvgContainer>
-          {/* <CircleLayer>
-            <Circle />
-            <Circle circleRadius="200" x="600" y="350" />
-          </CircleLayer> */}
-        </SvgContainer>
+        <SvgContainer />
       </Canvas>
     </div>
   );
