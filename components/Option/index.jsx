34578d44df8d763e93cd0e24cb3a908c4562efac
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-25 16:04:56 -0400

    add onliner class

--- components/Option/index.jsx
+++ components/Option/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function Option({ title = "", children }) {
+export default function Option({ title = "", oneline = false, children }) {
   return (
-    <div className="option">
+    <div className={`${!oneline ? `option` : `option-online`}`}>
       {title.length > 0 ? <div className="option__title">{title}</div> : null}
       <div className="option__container">{children}</div>
     </div>
