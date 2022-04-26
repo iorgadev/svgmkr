8ae2aabcede9b9e3c4b1d6a7ad54b2c6a1261578
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-26 00:05:06 -0400

    add state managed shape parameters

--- components/Circle/Circle.jsx
+++ components/Circle/Circle.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import { useStore } from "@/store/index";
 
-export default function Circle({ circleRadius = 100, x = 200, y = 200 }) {
+export default function Circle({
+  index,
+  circleRadius = 100,
+  x = 200,
+  y = 200,
+}) {
   const strokeColor = useStore((state) => state.strokeColor);
   const strokeWidth = useStore((state) => state.strokeWidth);
+  const shape = useStore((state) => state.shapes[index]);
+
   const [radius, setRadius] = useState(circleRadius); // might want to resize later
   const [position, setPosition] = useState({ x: x, y: y });
   return (
     <circle
       r={circleRadius}
-      cx={position.x}
-      cy={position.y}
+      cx={shape.x}
+      cy={shape.y}
       stroke={strokeColor}
       strokeWidth={strokeWidth}
     />
