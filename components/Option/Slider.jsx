958992b2b789ac0a994c98eb02c1a0b0a2cc6ce3
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-04-26 01:52:53 -0400

    style Slider component, wip

--- components/Option/Slider.jsx
+++ components/Option/Slider.jsx
@@ -11,6 +11,7 @@ export default function Slider({
   smallIcon = false,
   steps = 1,
 }) {
+  const widthPercentage = (value / max) * 100;
   return (
     <Option title={title}>
       <div className="flex-none rounded-full p-1.5 bg-neutral-700">
@@ -18,7 +19,7 @@ export default function Slider({
           className={`option__icon-round ${smallIcon ? `icon-small` : ``}`}
         />
       </div>
-      <div className="flex items-center justify-center option__slider">
+      <div className="relative flex items-center justify-start option__slider">
         <input
           type="range"
           min={min}
@@ -28,6 +29,13 @@ export default function Slider({
           onChange={(e) => onChange(e.target.value)}
           className="w-full"
         />
+        <div className="absolute w-full h-1.5 rounded-l-full rounded-r-full bg-neutral-700"></div>
+        <div
+          className={`absolute h-1.5 rounded-l-full rounded-r-full bg-neutral-500 flex items-center justify-end`}
+          style={{ width: `${widthPercentage}%` }}
+        >
+          {/* <div className="relative w-5 h-5 rounded-full pointer-events-none bg-neutral-600 cursor-ew-resize"></div> */}
+        </div>
       </div>
       <div className="flex-none rounded-full p-1.5 bg-neutral-700">
         <RightIcon className="option__icon-round" />
